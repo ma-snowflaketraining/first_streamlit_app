999528f235401d675bcc903a30b666b1bb5399e0
ma-snowflaketraining <130389041+ma-snowflaketraining@users.noreply.github.com>
2023-04-11 16:34:39 -0400

    Update streamlit_app.py
    correcting execute code

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,4 +50,4 @@ add_my_fruit = streamlit.text_input('What fruit would you like information about
 streamlit.write('Thanks for adding ', add_my_fruit)
 
 # this will not work correctly, but just go with it for now
-my_cur.execute("insert into fruit_load_list values ('from streamlit')"
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
